ce2ffc72e1c1ebb7193eeb2ee72544ed23548d53
Vincent <v3rdugo@yahoo.com>
2022-08-08 17:17:34 -0700

    inherit from openzeppelin ERC20 contract

--- contracts/OpenZeppelinToken.sol
+++ contracts/OpenZeppelinToken.sol
@@ -1,2 +1,12 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.7;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract OpenZeppelinToken is ERC20
+{
+    constructor() ERC20("OpenZeppelinToken", "OZT")
+    {
+        
+    }
+}
