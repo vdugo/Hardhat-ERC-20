b9cb024b6cec643b64a0ba0685c29ecaf309aed0
Vincent <v3rdugo@yahoo.com>
2022-08-08 17:01:15 -0700

    minimal functionality

--- contracts/ManualToken.sol
+++ contracts/ManualToken.sol
@@ -1,3 +1,23 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.7;
 
+contract ManualToken
+{
+    uint256 totalSupply;
+    mapping(address => uint256) public balanceOf;
+    mapping(address => mapping(address => uint256)) public allowance;
+
+    function _transfer(address _from, address _to, uint256 _amount) public
+    {
+        balanceOf[from] -= amount;
+        balanceOf[to] += amount;
+    }
+
+    function transferFrom(address _from, address _to, address _amount) public bool returns(bool success)
+    {
+        require(_amount <= allowance[_from][msg.sender]);
+        allowance[_from][msg.sender] -= _amount;
+        _transfer(_from, _to, _value);
+        return true;
+    }
+}
